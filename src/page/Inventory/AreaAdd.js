6165a2bdbf770c170caadb717c57b9903b3b92cd
Elias <jmenafonseca28@gmail.com>
2023-10-22 08:36:33 -0600

    Modulo inventario completisimo

---
+++ src/page/Inventory/AreaAdd.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+import { createArea, updateArea, getArea } from '../../service/InventoryApi/AreaApi';
+
+function AreaAdd() {
+    const [isEditing, setIsEditing] = useState(false);
+    const [idE, setIdE] = useState(0);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const area1 = form.area.value;
+
+        const newArea = {
+            inventoryId: localStorage.getItem("idInventory"),
+            area1
+        };
+
+        console.log(newArea);
+
+        try {
+            if (!isEditing) {
+                const response = await createArea(newArea);
+                console.log(response);
+                alert('Area creado exitosamente');
+            } else {
+                newArea.id = idE;
+                const response = await updateArea(newArea);
+                console.log(response);
+                alert('Area actualizado exitosamente');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+
+        window.location.href = "/inventory/area";
+    }
+
+    const getAreaE = async (id) => {
+        const res = await getArea(id);
+        if (res) {
+            setForm(res);
+        }
+    }
+
+    function setForm(area) {
+        document.getElementById("area").value = area.area1;
+    }
+
+    useEffect(() => {
+        if (localStorage.getItem("id")) {
+            getAreaE(localStorage.getItem("id"));
+            setIsEditing(true);
+            setIdE(localStorage.getItem("id"));
+            localStorage.removeItem("id");
+        }
+    }, []);
+
+    return (
+        <div className='container pt-5'>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="area">
+                    <Form.Label>Area</Form.Label>
+                    <Form.Control type="text" placeholder="Area" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    {isEditing ? 'Actualizar' : 'Crear'}
+                </Button>
+            </Form>
+        </div>
+    );
+}
+
+export default AreaAdd;
--- src/service/UsersApi/UserApi.js
+++ src/service/UsersApi/UserApi.js
@@ -5,52 +5,49 @@ const url = 'https://localhost:7210/api/Userrs/';
 export async function getUserrs() {
     try {
         const response = await axios.get(url + 'getAll/');
-        //console.log(response.data);
-        return response.data; // Devuelve el contenido JSON de la respuesta
+        return response.data; 
     } catch (error) {
         console.error('Error al obtener los datos:', error);
-        throw error; // Puedes manejar el error aquí o lanzarlo para que se maneje en otro lugar.
+        throw error; 
     }
 }
 
 export async function getUserr(id) {
     try {
         const response = await axios.get(url + 'getUser/' + id);
-        return response.data; // Devuelve el contenido JSON de la respuesta
+        return response.data;
     } catch (error) {
         console.error('Error al obtener los datos:', error);
-        throw error; // Puedes manejar el error aquí o lanzarlo para que se maneje en otro lugar.
+        throw error; 
     }
 }
 
 export async function createUser(data) {
     try {
         const response = await axios.post(url + 'createUser/', data);
-        //console.log(response.data);
-        return response.data; // Devuelve el contenido JSON de la respuesta
+        return response.data;
     } catch (error) {
         console.error('Error al crear el usuario:', error);
-        throw error; // Puedes manejar el error aquí o lanzarlo para que se maneje en otro lugar.
+        throw error; 
     }
 }
 
 export async function updateUserr(data) {
     try {
         const response = await axios.put(url + 'updateUser/', data);
-        //console.log(response.data);
-        return response.data; // Devuelve el contenido JSON de la respuesta
+        return response.data;
     } catch (error) {
         console.error('Error al actualizar el usuario:', error);
-        throw error; // Puedes manejar el error aquí o lanzarlo para que se maneje en otro lugar.
+        throw error; 
     }
 }
 
 export async function deleteUserr(id) {
     try {
         const response = await axios.delete(url + 'deleteUser/' + id);
-        return response.data; // Devuelve el contenido JSON de la respuesta
+        return response.data;
     } catch (error) {
         console.error('Error al eliminar el usuario:', error);
-        throw error; // Puedes manejar el error aquí o lanzarlo para que se maneje en otro lugar.
+        throw error;
     }
 }
