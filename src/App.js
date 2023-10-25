12c5e19cd0f38c58a6986d3fffe068cf8fb2a1ce
ProfesorAlbino <jeikelyamir@gmail.com>
2023-10-25 10:35:41 -0600

    ComputerEquipments y SanctionsReport

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './layout/Navbar';
 import User from './page/User/User';
-
-
+import ComputerEquipments from './page/ComputerEquipments/ListComputerEquipments';
+import ModifyComputerEquipments from './page/ComputerEquipments/ModifyComputerEquipments';
+import AddComputerEquipments from './page/ComputerEquipments/AddComputerEquipments';
+import ListSanctionsReport from './page/SanctionsReport/ListSanctionsReport';
+import AddSanctionsReport from './page/SanctionsReport/AddSanctionsReport';
 
 function App() {
   return (
@@ -13,6 +16,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/users" element={<User />} />
+          <Route exact path="/ListComputerEquipments" element={<ComputerEquipments />} />
+          <Route exact path="/ModifyComputerEquipments" element={<ModifyComputerEquipments />} />
+          <Route exact path="/AddComputerEquipments" element={<AddComputerEquipments />} />
+          <Route exact path="/ListSanctionsReport" element={<ListSanctionsReport />} />
+          <Route exact path="/AddSanctionsReport" element={<AddSanctionsReport />} />
           {/* <Route exact path="/" element={<Home />} /> */}
 
         </Routes>
--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -6,11 +6,13 @@ export default function Navbar() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-primary navbar-custom">
-                <a className="navbar-brand" href="#">Sistema de Prestamos</a>
+                <a className="navbar-brand" href="/">Sistema de Prestamos</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <Link type="button" className="btn btn-outline-light" to="/addForm">Agrega las opciones de los demas aqui</Link>
+                <Link type="button" className="btn btn-outline-light" to="/addForm">info</Link>
+                <Link type="button" className="btn btn-outline-light" to="/ListComputerEquipments">Equipo informatico</Link>
+                <Link type="button" className="btn btn-outline-light" to="/ListSanctionsReport">Sanciones</Link>
             </nav>
         </div>
     )
