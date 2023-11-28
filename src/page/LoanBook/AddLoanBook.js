fc1a7c6e7cb87587b1ee42fe82ac292324572647
Jonnathan Garcia <jonnathangrclr@gmail.com>
2023-11-27 19:12:28 -0600

    correcion de bugs

--- src/page/LoanBook/AddLoanBook.js
+++ src/page/LoanBook/AddLoanBook.js
@@ -2,14 +2,17 @@ import { React, useState, useEffect, useRef } from "react";
 import { createLoan } from "../../service/LoanApi/LoanApi";
 import { createLoanBook } from "../../service/LoanBookApi/LoanBookApi";
 import { getById } from '../../service/TitlesApi/TitleApi';
-import { encryptAES,decryptAES } from '../../scripts/AES-256';
+import { encryptAES, decryptAES } from '../../scripts/AES-256';
+import { FormatterDateToForms } from '../../scripts/FormatterDate';
+
 
 export default function LoanBook() {
 
     const idCopy = parseInt(decryptAES(localStorage.getItem("idCopy")));
-    const idTitle = localStorage.getItem("idTitle");
-    const subLibrary = localStorage.getItem("subLibrary");
+    const idTitle = parseInt(decryptAES(localStorage.getItem("idTitle")));
+    const subLibrary = decryptAES(localStorage.getItem("subLibrary"));
     const Initdate = useRef();
+    const EndDate = useRef();
 
     const [loan, setLoans] = useState({
         id: 0,
@@ -22,7 +25,7 @@ export default function LoanBook() {
         id: 0,
         idLoan: 0, /*por la respuesta del endpoint del loan*/
         idCopy: idCopy, /*por localstorage*/
-        idLibraryUser: 3, /*por localstorage*/
+        idLibraryUser: JSON.parse(sessionStorage.getItem("user")).idLibraryUser, /*por localstorage*/
         title: "",
         photocopyCharge: 0,
         subLibrary: subLibrary,
@@ -31,15 +34,17 @@ export default function LoanBook() {
         state: 0
     });
 
+    if (Initdate.current) {
+        Initdate.current.addEventListener("change", (e) => {
+            if (EndDate.current.value < e.target.value) {
+                EndDate.current.value = e.target.value;
+            }
+        });
+    }
+    
     useEffect(() => {
         const currentDate = new Date();
         const formattedDate = currentDate.toISOString().split('T')[0];
-
-        if (Initdate.current) {
-            Initdate.current.value = new Date().toISOString().split('T')[0];
-        }
-
-
         setLoans({ ...loan, registerDate: formattedDate });
         getById(idTitle).then((result) => {
             setLoanBooks({ ...loanBook, title: result.title1 });
@@ -48,6 +53,7 @@ export default function LoanBook() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        loan.endDate = EndDate.current.value;
         createLoan(loan)
             .then((response) => {
                 loanBook.idLoan = response.id;
@@ -57,10 +63,12 @@ export default function LoanBook() {
                 createLoanBook(loanBook)
                     .then((response) => {
                         console.log(loanBook);
-                        window.location.href = "/listLoanBook/" + 3;
+                        window.location.href = "/listLoanBook";
                     }).catch((error) => {
                         console.log("error al crear el loanBook");
                     });
+            }).catch((error) => {
+                console.log("error al crear el loan");
             });
     };
 
@@ -89,7 +97,7 @@ export default function LoanBook() {
                                 type="number"
                                 className="form-control border border-primary"
                                 required
-                                onChange={(e) => setLoanBooks({ ...loanBook, photocopyCharge: e.target.value })}
+                                onChange={(e) => setLoanBooks({ ...loanBook, photocopyCharge: parseInt(e.target.value) })}
                             />
                             <label className="form-label ms-2">Cargo por Fotocopias</label>
                         </div>
@@ -109,7 +117,8 @@ export default function LoanBook() {
                                 type="number"
                                 className="form-control border border-primary"
                                 required
-                                onChange={(e) => setLoanBooks({ ...loanBook, limitFines: e.target.value })}
+                                max={9999}
+                                onChange={(e) => setLoanBooks({ ...loanBook, limitFines: parseInt(e.target.value) })}
                             />
                             <label className="form-label ms-2">Limite de Multas</label>
                         </div>
@@ -120,17 +129,20 @@ export default function LoanBook() {
                                 type="date"
                                 className="form-control border border-primary"
                                 required
-                                min={loan.registerDate}
+                                min={FormatterDateToForms(loan.registerDate)}
+                                value={FormatterDateToForms(loan.startDate)}
                                 onChange={(e) => setLoans({ ...loan, startDate: e.target.value })}
                             />
                             <label className="form-label ms-2">Fecha inicio</label>
                         </div>
                         <div className="mb-4 form-floating col-lg-4 col-md-4 col-sm-6 col-xs-12">
                             <input
+                            ref={EndDate}
                                 type="date"
                                 className="form-control border border-primary"
                                 required
-                                min={loan.startDate}
+                                min={FormatterDateToForms(loan.startDate)}
+                                value={FormatterDateToForms(loan.endDate)}
                                 onChange={(e) => setLoans({ ...loan, endDate: e.target.value })}
                             />
                             <label className="form-label ms-2">Fecha de devolución</label>
