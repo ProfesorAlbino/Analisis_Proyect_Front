4df5379c4c6c95e41ba5884fc729bdd363096c40
Elias Mena <jmenafonseca28@gmail.com>
2023-12-04 01:22:53 -0600

    t2

--- src/Test/Login.test.js
+++ src/Test/Login.test.js
@@ -19,7 +19,9 @@ describe('Login Page', function() {
     await driver.findElement(By.id('userPass')).sendKeys('1234567');
     await driver.findElement(By.id('submit')).click();
 
-    await driver.wait(until.urlIs('http://localhost:3000/'), 100000);
+    //await driver.wait(until.urlIs('http://localhost:3000/'), 100000);
+
+    await driver.sleep(5000);
 
     //const user = sessionStorage.getItem('user');
     const dr = await driver.executeScript('return sessionStorage.getItem("user")');
